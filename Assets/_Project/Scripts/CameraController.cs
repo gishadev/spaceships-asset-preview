31a30999240f5660845d48d75c85a7da9c5db9c1
Bohdan Tsaplia <bohdan.tsaplia@udtech.global>
2023-09-14 00:51:40 +0300

    import sprites & spaceship flying

---
+++ Assets/_Project/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace gishadev
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private float cameraZoomSpeed = 1f;
+        [SerializeField] private float moveSpeed = 1f;
+        
+        private Camera _camera;
+        private float _targetSize;
+
+        public float MoveSpeed => moveSpeed;
+
+        private void Awake()
+        {
+            _camera = GetComponent<Camera>();
+        }
+
+        private void Update()
+        {
+            _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, _targetSize, Time.deltaTime * cameraZoomSpeed);
+            transform.Translate(Vector3.up * (MoveSpeed * Time.deltaTime));
+        }
+
+        public void SetTargetSize(float targetSize)
+        {
+            _targetSize = targetSize;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Spaceship.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace gishadev
+{
+    public class Spaceship : MonoBehaviour
+    {
+        [SerializeField] private float zoomFactor = 5f;
+        [SerializeField] private float rotationAmplitude = 30f;
+        [SerializeField] private float xMoveOffset = 1f;
+
+        private Vector3 _lastPos;
+        
+        private void OnEnable()
+        {
+            FindObjectOfType<CameraController>().SetTargetSize(zoomFactor);
+        }
+
+        private void Update()
+        {
+            transform.localPosition = Vector3.right * (Mathf.Sin(Time.time) * xMoveOffset);
+            transform.localRotation = Quaternion.Euler(Vector3.forward * (Mathf.Sin(Time.time) * rotationAmplitude));
+            
+            _lastPos = transform.position;
+        }
+    }
+}
