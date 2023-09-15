90d18c96544a6d371ec391f03b1d6bfef2075472
Bohdan Tsaplia <bohdan.tsaplia@udtech.global>
2023-09-16 01:09:39 +0300

    add turrets

---
+++ Assets/_Project/Scripts/Turret.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace gishadev
+{
+    public class Turret : MonoBehaviour
+    {
+        private void Update()
+        {
+            var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var direction = (worldPos - transform.position).normalized;
+
+            var rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90f;
+            transform.rotation = Quaternion.Euler(Vector3.forward * rotZ);
+        }
+    }
+}
